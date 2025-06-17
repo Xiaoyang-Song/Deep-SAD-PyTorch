fe1a649a23668bb5ad3faf751c2a6d4411a152be
Xiaoyang-Song <xysong@umich.edu>
2025-06-17 02:24:39 -0400

    Update: New results

--- jobs/schedule.sh
+++ jobs/schedule.sh
@@ -33,27 +33,27 @@
 # sbatch jobs/cifar10-svhn/2048.sh
 # sbatch jobs/cifar10-svhn/4096.sh
 
-# sbatch jobs/mnist/8.sh
-# sbatch jobs/mnist/16.sh
-# sbatch jobs/mnist/32.sh
-# sbatch jobs/mnist/64.sh
-# sbatch jobs/mnist/128.sh
-# sbatch jobs/mnist/256.sh
-# sbatch jobs/mnist/512.sh
-# sbatch jobs/mnist/1024.sh
-# sbatch jobs/mnist/2048.sh
-# sbatch jobs/mnist/4096.sh
+sbatch jobs/mnist/8.sh
+sbatch jobs/mnist/16.sh
+sbatch jobs/mnist/32.sh
+sbatch jobs/mnist/64.sh
+sbatch jobs/mnist/128.sh
+sbatch jobs/mnist/256.sh
+sbatch jobs/mnist/512.sh
+sbatch jobs/mnist/1024.sh
+sbatch jobs/mnist/2048.sh
+sbatch jobs/mnist/4096.sh
 
 # sbatch jobs/mnist-fashionmnist/8.sh
-sbatch jobs/mnist-fashionmnist/16.sh
-sbatch jobs/mnist-fashionmnist/32.sh
-sbatch jobs/mnist-fashionmnist/64.sh
-sbatch jobs/mnist-fashionmnist/128.sh
-sbatch jobs/mnist-fashionmnist/256.sh
-sbatch jobs/mnist-fashionmnist/512.sh
-sbatch jobs/mnist-fashionmnist/1024.sh
-sbatch jobs/mnist-fashionmnist/2048.sh
-sbatch jobs/mnist-fashionmnist/4096.sh
+# sbatch jobs/mnist-fashionmnist/16.sh
+# sbatch jobs/mnist-fashionmnist/32.sh
+# sbatch jobs/mnist-fashionmnist/64.sh
+# sbatch jobs/mnist-fashionmnist/128.sh
+# sbatch jobs/mnist-fashionmnist/256.sh
+# sbatch jobs/mnist-fashionmnist/512.sh
+# sbatch jobs/mnist-fashionmnist/1024.sh
+# sbatch jobs/mnist-fashionmnist/2048.sh
+# sbatch jobs/mnist-fashionmnist/4096.sh
 
 
 # DONE
