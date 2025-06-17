0775cfed79d4107cc22f0eb7230eecbe1216111d
Xiaoyang-Song <xysong@umich.edu>
2025-06-16 23:46:37 -0400

    Sync

--- src/summary.sh
+++ src/summary.sh
@@ -8,6 +8,6 @@ python summarize.py --experiment fmnist-R2 --regime Imbalanced
 python summarize.py --experiment svhn --regime Balanced
 python summarize.py --experiment svhn-R2 --regime Imbalanced
 
-# python summarize.py --experiment cifar10-svhn --regime Balanced
-# python summarize.py --experiment mnist --regime Balanced
+python summarize.py --experiment cifar10-svhn --regime Balanced
+python summarize.py --experiment mnist --regime Balanced
 # python summarize.py --experiment mnist-fashionmnist --regime Balanced
